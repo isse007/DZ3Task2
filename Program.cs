ba075bee80490a71c68767c4977855853276d885
Simon Isupov <is_se@mail.ru>
2023-12-15 19:25:05 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 2: Задайте массив на 10 целых чисел. 
+// Напишите программу, которая определяет количество чётных чисел в массиве.
+
+int[] array = new int[10];
+for (int i = 0; i < array.Length; i++)
+{
+System.Console.WriteLine("Введите числа массива");
+array[i] = Convert.ToInt32(Console.ReadLine());    
+}
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    count = count + 1;
+}
+System.Console.WriteLine($"Количество четных чисел массива = {count}");
